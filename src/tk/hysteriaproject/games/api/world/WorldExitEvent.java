923d9bacda8445287ef9be4cd55d5c92d9fe433f
KingBowser <backseat.rapist@gmail.com>
2013-02-15 17:09:41 -0600

    Added events

---
+++ src/tk/hysteriaproject/games/api/world/WorldExitEvent.java
@@ -0,0 +1,55 @@
+package tk.hysteriaproject.games.api.world;
+
+import tk.hysteriaproject.api.spec.event.EventPacket;
+import tk.hysteriaproject.games.api.world.entity.Entity;
+
+/**
+ * Published when an entity enters the world
+ * Creator: KingBowser
+ * Date: 2/15/13
+ * Time: 4:55 PM
+ * Refer to LICENSE for licensing information
+ */
+public class WorldExitEvent extends EventPacket {
+
+    // Variables
+
+    /**
+     * World entered by the entity
+     */
+    private final World worldAccessed;
+
+    /**
+     * Entity entering the world
+     */
+    private final Entity entityInWorld;
+
+    // Constructors
+
+    public WorldExitEvent(World entering, Entity enterer) {
+
+        worldAccessed = entering;
+
+        entityInWorld = enterer;
+
+    }
+
+    // Methods
+
+    /**
+     * Get the entity in question
+     * @return entity
+     */
+    public Entity getEntity() {
+        return entityInWorld;
+    }
+
+    /**
+     * Get the world in question
+     * @return world
+     */
+    public World getWorld() {
+        return worldAccessed;
+    }
+
+}
