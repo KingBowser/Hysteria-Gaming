d198ed1dde88f3c848587dc5cc009301ae33224f
KingBowser <backseat.rapist@gmail.com>
2013-02-15 23:47:11 -0600

    Defined Inventories and their contents

---
+++ src/tk/hysteriaproject/games/api/elements/inventory/Inventory.java
@@ -0,0 +1,34 @@
+package tk.hysteriaproject.games.api.elements.inventory;
+
+/**
+ * Describes a slot-based inventory in the abstract
+ * Creator: KingBowser
+ * Date: 2/15/13
+ * Time: 10:51 PM
+ * Refer to LICENSE for licensing information
+ */
+public interface Inventory {
+
+    /**
+     * Get the size of the inventory in slots
+     *
+     * @return size
+     */
+    public int getSize();
+
+    /**
+     * Should return the space currently occupied
+     *
+     * @return used space count
+     */
+    public int getSpaceOccupied();
+
+    /**
+     * Get the slot for a specific slot-position
+     *
+     * @param slotID ID of the slot
+     * @return slot at position of ID
+     */
+    public Slot getSlot(int slotID);
+
+}
