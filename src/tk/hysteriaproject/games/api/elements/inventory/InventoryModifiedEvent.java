b8a858f0128c3d675bc3ddc80676cd9ad9885d4d
KingBowser <backseat.rapist@gmail.com>
2013-02-18 21:48:25 -0600

    Added an event to describe the modification of an Inventory

---
+++ src/tk/hysteriaproject/games/api/elements/inventory/InventoryModifiedEvent.java
@@ -0,0 +1,41 @@
+package tk.hysteriaproject.games.api.elements.inventory;
+
+import tk.hysteriaproject.api.spec.event.EventPacket;
+
+/**
+ * Published when an inventory is changed
+ * Creator: KingBowser
+ * Date: 2/18/13
+ * Time: 9:13 PM
+ * Refer to LICENSE for licensing information
+ */
+public class InventoryModifiedEvent extends EventPacket {
+
+    // Variables
+
+    /**
+     * Inventory that was modified
+     */
+    private final Inventory inventoryThatWasModified;
+
+    // Constructors
+
+
+    public InventoryModifiedEvent(Inventory inventory) {
+
+        inventoryThatWasModified = inventory;
+
+    }
+
+    // Methods
+
+    /**
+     * Get the inventory that was modified
+     *
+     * @return inventory
+     */
+    public Inventory getInventory() {
+        return inventoryThatWasModified;
+    }
+
+}
